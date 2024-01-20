b122de8c33322734d8e8ff972f29690bf3418873
Gokul Raj <42057165+gokul98raj@users.noreply.github.com>
2024-01-20 11:19:51 +0530

    finalbuild

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "db_endpoint" {
-  value = aws_db_instance.app_rds.endpoint
-}
-
 output "alb_url" {
-  value = aws_lb.app_load_balancer.dns_name
+  value = "http://${aws_lb.app_load_balancer.dns_name}"
 }
