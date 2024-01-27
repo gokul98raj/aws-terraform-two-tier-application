b28f5dddfbc1dcaed2279da94b1811d0c22f262c
Gokul Raj <42057165+gokul98raj@users.noreply.github.com>
2024-01-27 19:38:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
+/*
 terraform {
   backend "s3" {
     encrypt = true
-    bucket = "lock-test-bucket-tf"
-    dynamodb_table = "lock_test_dynamo"
+    bucket = "BUCKET_NAME"
+    dynamodb_table = "TABLE_NAME"
     key = "terraform.tfstate"
     region = "ap-south-1"
   }
-}
+} */
